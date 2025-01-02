ae260fb2a8d1742187eeaeaf91dfe73537c8dd13
Mohamed Nagoor S <97577086+MohamedNagoorS@users.noreply.github.com>
2025-01-02 19:08:27 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -54,6 +54,53 @@ public class LoginAutomationTest {
             driver.quit();
         }
     }
+     @Test
+    public void testSuccessfulLogin2() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            LoginAutomation loginAutomation = new LoginAutomation(driver);
+
+            // Navigate to the login page
+            loginAutomation.navigateToLoginPage("https://the-internet.herokuapp.com/login");
+
+            // Perform login with valid credentials
+            loginAutomation.login2("tomsmith", "SuperSecretPassword!");
+            
+
+            // Validate successful login
+            String flashMessage = loginAutomation.getFlashMessage();
+            assertTrue(flashMessage.contains("You logged into a secure area!"));
+            
+        } finally {
+            driver.quit();
+        }
+    }
+ @Test
+    public void testSuccessfulLogin3() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            LoginAutomation loginAutomation = new LoginAutomation(driver);
+
+            // Navigate to the login page
+            loginAutomation.navigateToLoginPage("https://the-internet.herokuapp.com/login");
+
+            // Perform login with valid credentials
+            loginAutomation.login3("tomsmith", "SuperSecretPassword!");
+            
+
+            // Validate successful login
+            String flashMessage = loginAutomation.getFlashMessage();
+            assertTrue(flashMessage.contains("You logged into a secure area!"));
+            
+        } finally {
+            driver.quit();
+        }
+    }
+
 
     @Test
     public void testFailedLogin() {
