5763a8d4bf566aa75f7f8825e2407e2bb0798caa
Mohamed Nagoor S <97577086+MohamedNagoorS@users.noreply.github.com>
2025-01-02 18:54:20 +0530

    Update LoginAutomation.java

--- src/main/java/com/example/automation/LoginAutomation.java
+++ src/main/java/com/example/automation/LoginAutomation.java
@@ -25,6 +25,15 @@ public class LoginAutomation {
         passwordField.sendKeys(password);
         loginButton.click();
     }
+    public void login1(String username, String password) {
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.cssSelector("button.radius"));
+
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
 
     public String getFlashMessage() {
         return driver.findElement(By.cssSelector(".flash")).getText();
