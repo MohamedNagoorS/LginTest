8e0ce4d08f9dcd7388d8c4fb5da0ddbd1d5c83a6
Mohamed Nagoor S <97577086+MohamedNagoorS@users.noreply.github.com>
2025-01-03 11:10:02 +0530

    Update LoginAutomation.java

--- src/main/java/com/example/automation/LoginAutomation.java
+++ src/main/java/com/example/automation/LoginAutomation.java
@@ -1,12 +1,11 @@
 package com.example.automation;
 
-import java.time.Duration;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 class LoginAutomation {
 
